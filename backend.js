b9a1688780457e4e313e55a5f2c6bc97d2316c01
RottenKiwi <markkittiphat@gmail.com>
2023-05-24 20:24:08 +0700

    Added randomiser and functions

--- backend.js
+++ backend.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const data = require("./data.json");
+const path = require("path");
 
 function decodeChapters(input) {
   let arr = [];
@@ -13,7 +14,7 @@ function decodeChapters(input) {
 }
 
 // GET request handler
-app.get("/example", (req, res) => {
+app.get("/vocab", (req, res) => {
   const queryParam = req.query.code;
 
   let chapters = decodeChapters(Number(req.query.code));
@@ -26,6 +27,23 @@ app.get("/example", (req, res) => {
   var item = vocabArr[Math.floor(Math.random() * vocabArr.length)];
 
   res.send(item);
+});
+
+app.use(express.static(path.join(__dirname)));
+
+// GET request handler for HTML file
+app.get("/html", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// GET request handler for CSS file
+app.get("/css", (req, res) => {
+  res.sendFile(path.join(__dirname, "style.css"));
+});
+
+// GET request handler for JavaScript file
+app.get("/js", (req, res) => {
+  res.sendFile(path.join(__dirname, "script.js"));
 });
 
 // Start the server
--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const canvas = document.getElementById("canvas");
 // Get the 2D drawing context
 const context = canvas.getContext("2d");
 
+// Request code for vocab
+let code = 0;
+const totalChapter = 2;
+let currentWord, currentPinyin, currentChinese;
+let showPinyin = false,
+  showChinese = false;
+
 adjustCanvasSize();
 clearCanvas();
 
@@ -11,7 +18,7 @@ clearCanvas();
 let currentX = 0;
 let currentY = 0;
 let isDrawing = false;
-let strokeSize = 10; // Initial stroke size
+let strokeSize = 10;
 
 // Event listeners for mouse events
 canvas.addEventListener("mousedown", startDrawing);
@@ -19,14 +26,12 @@ canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("mouseup", stopDrawing);
 canvas.addEventListener("mouseout", stopDrawing);
 
-// Function to start drawing
 function startDrawing(event) {
   isDrawing = true;
   currentX = event.clientX - canvas.offsetLeft;
   currentY = event.clientY - canvas.offsetTop;
 }
 
-// Function to draw
 function draw(event) {
   if (!isDrawing) return;
   const x = event.clientX - canvas.offsetLeft;
@@ -43,12 +48,10 @@ function draw(event) {
   currentY = y;
 }
 
-// Function to stop drawing
 function stopDrawing() {
   isDrawing = false;
 }
 
-// Function to change stroke size
 function changeStrokeSize(size) {
   strokeSize = size;
 }
@@ -64,12 +67,63 @@ function clearCanvas() {
   context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
+function encodeChapters(chapter, state) {
+  let mask = 1 << --chapter;
+  code = (code & ~mask) | ((state ? 1 : 0) << chapter);
+
+  if (code == 0) code = (1 << totalChapter) - 1;
+}
+
+function nextWord() {
+  fetch(`/vocab?code=${code}`)
+    .then((res) => res.json())
+    .then((data) => {
+      currentWord = data[0];
+      currentPinyin = data[1];
+      currentChinese = data[2];
+      showChinese = true;
+      showPinyin = true;
+      toggleChinese();
+      togglePinyin();
+      document.querySelector("#vocab").textContent = currentWord;
+      document.querySelector("#pinyin").textContent = currentPinyin;
+      document.querySelector("#chinese").textContent = currentChinese;
+      clearCanvas();
+    });
+}
+
+function togglePinyin() {
+  showPinyin = !showPinyin;
+  document.querySelector("#pinyin").style.display = showPinyin
+    ? "inline"
+    : "none";
+}
+
+function toggleChinese() {
+  showChinese = !showChinese;
+  document.querySelector("#chinese").style.display = showChinese
+    ? "inline"
+    : "none";
+}
+
 window.addEventListener("resize", adjustCanvasSize);
 window.addEventListener("keypress", (e) => {
   switch (e.key) {
     case "E":
     case "e":
       clearCanvas();
+      break;
+    case "S":
+    case "s":
+      togglePinyin();
+      break;
+    case "D":
+    case "d":
+      toggleChinese();
+      break;
+    case "F":
+    case "f":
+      nextWord();
       break;
     default:
       return;
